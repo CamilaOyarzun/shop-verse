4af2b661bb62054d960bf21a20aa5d461809899b
camila <camilaoyarzun96@gmail.com>
2025-05-10 23:47:24 -0400

    first commit

---
+++ src/main/java/com/technova/shopverse/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.technova.shopverse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthCheckController {
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
